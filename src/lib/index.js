ef44d92e4c8fad9496f8e37d3ff6c77870597005
Jesse Lokerse <loke0017@hz.nl>
2023-11-28 10:23:46 +0100

    database werkend en linken met backend server

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,32 @@
-// place files you want to import through the `$lib` alias in this folder.
+let data = "niks";
+let thing = "";
+async function fetchData(id) {
+  try {
+    const response = await fetch("http://localhost:4000/recipes");
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    data = await response.json();
+
+    // Verwerk de ontvangen data
+    if(id === !'all'){
+        getUserData(id-1);
+    } else {
+         getAllUserData()
+    }
+  } catch (error) {
+    // Handel fouten af
+    console.error("Fetch error:", error);
+  }
+}
+// Roep de functie aan
+fetchData('all');
+function getUserData(id) {
+  console.log(data[id]);
+}
+function getAllUserData(){
+    console.log(data)
+}
+
