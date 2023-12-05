043616a8d48757ceaf4fec531dd54318fe5056c5
Armin Elshout <armin.elshout@gmail.com>
2023-12-05 14:33:47 +0100

    jaaaaaaa

--- src/lib/components/LoginForm.svelte
+++ src/lib/components/LoginForm.svelte
@@ -8,44 +8,74 @@
     };
   
     async function handleSubmit(event) {
-      event.preventDefault();
-      const newUser = {
-        name: form.name,
-        email: form.email,
-        password: form.password
-      };
-  
-      // Post the new user data to the backend
-      await postData('users', newUser);
-  console.log(newUser)
-      // Optionally, reset form fields after successful submission
-      form.name = '';
-      form.email = '';
-      form.password = '';
-    }
-  
-    async function postData(endpoint, data) {
-      try {
-        const response = await fetch(`http://localhost:4002/${endpoint}`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(data),
-        });
-  
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-  
-        // Optionally handle the response from the server
-        const result = await response.json();
-        console.log('Data posted:', result);
-      } catch (error) {
-        console.error('Fetch error:', error);
-        // Optionally handle errors here
+  event.preventDefault();
+
+  if (form.password !== form.passwordConfirm) {
+    form.error = "Passwords don't match";
+    return;
+  }
+
+  const newUser = {
+    name: form.name,
+    email: form.email,
+    password: form.password
+  };
+  {window.location.replace("http://localhost:5173/dashboard")}
+
+  try {
+    // Post the new user data to the backend
+    const response = await postData('users', newUser);
+
+    if (response) {
+      if (response.message === 'User registered') {
+        console.log('User registered. UserID:', response.userId);
+        // Reset form fields after successful submission
+        form.name = '';
+        form.email = '';
+        form.password = '';
+        form.passwordConfirm = '';
+      } else {
+        // Handle unexpected response
+        console.error('Unexpected response:', response);
+        form.error = 'Registration failed. Unexpected response.';
       }
+    } else {
+      // Handle empty response
+      console.error('Empty response received.');
+      form.error = 'Registration failed. Empty response received.';
     }
+  } catch (error) {
+    console.error('Registration failed:', error);
+    form.error = 'Registration failed. Please try again.';
+  }
+}
+
+  
+async function postData(endpoint, data) {
+  try {
+    const response = await fetch(`http://localhost:4002/${endpoint}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    // Log the entire response and the parsed JSON content
+    const result = await response.json();
+    console.log('Data posted:', result);
+    console.log('Response:', response);
+
+    return result; // Return the parsed JSON content
+  } catch (error) {
+    console.error('Fetch error:', error);
+    throw error; // Propagate the error
+  }
+}
   </script>
   
   <form on:submit={handleSubmit} class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
@@ -72,9 +102,8 @@
             <input bind:value={form.password} class="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none" type="password" id="password" placeholder="**********" required/>
         </div>
         <div class="mb-4">
-            <label for="passwordConfirm" class="block text-gray-700 text-sm font-bold mb-2">password Confirm</label>
-            <input class="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none" type="password" id="passwordConfirm" placeholder="**********" required/>
-        </div>
-        <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none">Register</button>
-</form>
-    
+            <label for="passwordConfirm" class="block text-gray-700 text-sm font-bold mb-2">Confirm Password</label>
+            <input bind:value={form.passwordConfirm} class="shadow border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none" type="password" id="passwordConfirm" placeholder="**********" required/>
+          </div>
+          <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none">Register</button>
+        </form>
