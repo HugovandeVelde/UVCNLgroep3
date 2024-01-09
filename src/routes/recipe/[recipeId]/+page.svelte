520d876b43d75704f314da95a90b678631dedb98
Jesse Lokerse <loke0017@hz.nl>
2024-01-09 12:27:24 +0100

    recipePage fini

--- src/routes/recipe/[recipeId]/+page.svelte
+++ src/routes/recipe/[recipeId]/+page.svelte
@@ -3,12 +3,14 @@
   import { page } from "$app/stores";
   import { requestedAllData } from "../../../lib/users";
   import { requestedAllRecipeData } from "../../../lib/index";
+  import { requestedAllIngredients } from "../../../lib/ingredients";
+  import { requestedAllSteps } from "../../../lib/steps";
   const recipeId = $page.params.recipeId;
   let recipe;
 </script>
 
 <main>
-  <p class='invisible'>{(recipe = requestedAllRecipeData[recipeId])}</p>
+  <p class="invisible">{(recipe = requestedAllRecipeData[recipeId])}</p>
   <table width="90%">
     <tr>
       <th colspan="5">
@@ -16,17 +18,17 @@
       </th>
     </tr>
     <tr>
-      <td colspan="5" class='center'>
+      <td colspan="5" class="center">
         {#each requestedAllData as user}
           {#if user.id === recipe.creator_id}
-            <h5 class='mb-8'>{user.name}</h5>
+            <h5 class="mb-8">{user.name}</h5>
           {/if}
         {/each}
       </td>
     </tr>
-    </table>
-    <div class='line'></div>
-    <table>
+  </table>
+  <div class="line"></div>
+  <table>
     <tr>
       <td class="logoVak">
         <img src="./../src/img/klok.png" alt="klok" class="logoImg" />
@@ -64,19 +66,43 @@
       <td colspan="2">{recipe.kitchenware}</td>
     </tr>
   </table>
-  <div class='line'></div>
+  <div class="line"></div>
+  <table width="90%">
+    <th colspan="5">
+      <h2>Ingredienten:</h2>
+    </th>
+    {#each requestedAllIngredients as ingredient}
+      {#if ingredient.recipeId === recipe.id}
+        <tr>
+          <td class="hoeveelheidVak">
+            <p>{ingredient.Hoeveelheid}</p>
+          </td>
+          <td>
+            <p class="ml-2">{ingredient.ingredientName}</p>
+          </td>
+        </tr>
+      {/if}
+    {/each}
+  </table>
+  <div class="line"></div>
+  <table width="90%">
+    <th colspan="5">
+      <h2>Bereidingswijze:</h2>
+    </th>
+    {#each requestedAllSteps as stap}
+      {#if stap.recipeId === recipe.id}
+        <tr>
+          <td VALIGN='top'>
+            <p>{stap.stepNr}.</p>
+          </td>
+          <td >
+            <p>{stap.instructie}</p>
+          </td>
+        </tr>
+      {/if}
+    {/each}
+  </table>
   <!-- <div>
-    <h2>[Ingredient name] [hoeveelheid]</h2>
-    <br />
-    <h2>[Ingredient name] [hoeveelheid]</h2>
-    <br />
-    <h2>[Ingredient name] [hoeveelheid]</h2>
-    <br />
-    <h2>[Ingredient name] [hoeveelheid]</h2>
-    <br />
-    <h2>[Ingredient name] [hoeveelheid]</h2>
-    <br />
-    <div>
       <h2>[step nr.]</h2>
       <h3>[step instruction]</h3>
       <h2>[step nr.]</h2>
@@ -104,12 +130,15 @@
     main {
       padding: 0px;
     }
-  .line {
-    height: 1px;
-    width: 95%;
-    margin-left: 2.5%;
-    border: 1px solid black;
-  }
+    .hoeveelheidVak {
+      text-align: right;
+    }
+    .line {
+      height: 1px;
+      width: 95%;
+      margin-left: 2.5%;
+      border: 1px solid black;
+    }
     h2 {
       font-size: 1.5rem;
       font-weight: bold;
@@ -127,20 +156,18 @@
     .logoImg {
       height: 20px;
       width: 20px;
-      display: block; 
-      margin-left: 70%; 
+      display: block;
+      margin-left: 70%;
       margin-right: auto;
-      
     }
     table {
       margin: 5%;
     }
-    .logoVak {
-      text-align: left;
+    .logoVak,
+    .hoeveelheidVak {
       width: 30%;
-      
     }
-    .center{
+    .center {
       text-align: center;
     }
   }
